6a64e02fc2f874c578598897374bfa330b2bb6bb
jorge-mejia13 <jorge_mejia@insideranken.org>
2025-05-22 17:21:23 -0500

    Finished Lab 4 for Ch 2-3

---
+++ Lab Assignments/CH02/Ch2-3Labs/Lab4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter number of days car was rented for: ");
+double numberOfDays = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter number of miles driven: ");
+double numberOfMiles = Convert.ToDouble(Console.ReadLine());
+
+const double CHARGE_PER_DAY = 100;
+const double CHARGE_PER_MILE = 0.50;
+
+double totalFee = (numberOfDays * CHARGE_PER_DAY) + (numberOfMiles * CHARGE_PER_MILE);
+
+Console.WriteLine("Your total for your trip is: {0:C}", totalFee);
