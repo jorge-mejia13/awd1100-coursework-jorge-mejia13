7d27341bdf8b77c69d11e5cdd1f09cc1b48128c9
jorge-mejia13 <jorge_mejia@insideranken.org>
2025-05-22 16:29:45 -0500

    CH2-3 Labs - Lab 1

---
+++ Lab Assignments/CH02/Ch2-3Labs/Lab1/Program.cs
@@ -0,0 +1,9 @@
+﻿const double KM_PER_MILE = 1.6;
+
+Console.Write("Enter your distance in miles please: ");
+
+double miles = Convert.ToDouble(Console.ReadLine());
+
+double kilometers = miles * KM_PER_MILE;
+
+Console.WriteLine("{0:0.0} miles is {1:0.0} kilometers", miles, kilometers);
