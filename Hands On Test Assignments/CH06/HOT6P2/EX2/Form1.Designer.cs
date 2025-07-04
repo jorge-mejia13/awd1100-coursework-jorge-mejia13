e2d107ac1c4179364accf5cd9f1fd76dca74f1e7
jorge-mejia13 <jorge_mejia@insideranken.org>
2025-07-04 03:01:11 -0500

    HOT6P2

---
+++ Hands On Test Assignments/CH06/HOT6P2/EX2/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace EX2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.qtInput = new System.Windows.Forms.TextBox();
+            this.orderBtn = new System.Windows.Forms.Button();
+            this.dcInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.codeAcceptLbl = new System.Windows.Forms.Label();
+            this.shirtCounterLbl = new System.Windows.Forms.Label();
+            this.subtotalLbl = new System.Windows.Forms.Label();
+            this.taxLbl = new System.Windows.Forms.Label();
+            this.totalLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(178, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quantity";
+            // 
+            // qtInput
+            // 
+            this.qtInput.Location = new System.Drawing.Point(318, 65);
+            this.qtInput.Name = "qtInput";
+            this.qtInput.Size = new System.Drawing.Size(157, 26);
+            this.qtInput.TabIndex = 1;
+            // 
+            // orderBtn
+            // 
+            this.orderBtn.Location = new System.Drawing.Point(508, 90);
+            this.orderBtn.Name = "orderBtn";
+            this.orderBtn.Size = new System.Drawing.Size(106, 36);
+            this.orderBtn.TabIndex = 2;
+            this.orderBtn.Text = "Order";
+            this.orderBtn.UseVisualStyleBackColor = true;
+            this.orderBtn.Click += new System.EventHandler(this.orderBtn_Click);
+            // 
+            // dcInput
+            // 
+            this.dcInput.Location = new System.Drawing.Point(318, 115);
+            this.dcInput.Name = "dcInput";
+            this.dcInput.Size = new System.Drawing.Size(157, 26);
+            this.dcInput.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(178, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Discount Code";
+            // 
+            // codeAcceptLbl
+            // 
+            this.codeAcceptLbl.AutoSize = true;
+            this.codeAcceptLbl.Location = new System.Drawing.Point(318, 183);
+            this.codeAcceptLbl.Name = "codeAcceptLbl";
+            this.codeAcceptLbl.Size = new System.Drawing.Size(0, 20);
+            this.codeAcceptLbl.TabIndex = 5;
+            // 
+            // shirtCounterLbl
+            // 
+            this.shirtCounterLbl.AutoSize = true;
+            this.shirtCounterLbl.Location = new System.Drawing.Point(178, 227);
+            this.shirtCounterLbl.Name = "shirtCounterLbl";
+            this.shirtCounterLbl.Size = new System.Drawing.Size(189, 20);
+            this.shirtCounterLbl.TabIndex = 6;
+            this.shirtCounterLbl.Text = "0 T-Shirts @ $13.75 each";
+            // 
+            // subtotalLbl
+            // 
+            this.subtotalLbl.AutoSize = true;
+            this.subtotalLbl.Location = new System.Drawing.Point(178, 279);
+            this.subtotalLbl.Name = "subtotalLbl";
+            this.subtotalLbl.Size = new System.Drawing.Size(95, 20);
+            this.subtotalLbl.TabIndex = 7;
+            this.subtotalLbl.Text = "Subtotal: $0";
+            // 
+            // taxLbl
+            // 
+            this.taxLbl.AutoSize = true;
+            this.taxLbl.Location = new System.Drawing.Point(178, 299);
+            this.taxLbl.Name = "taxLbl";
+            this.taxLbl.Size = new System.Drawing.Size(60, 20);
+            this.taxLbl.TabIndex = 8;
+            this.taxLbl.Text = "Tax: $0";
+            // 
+            // totalLbl
+            // 
+            this.totalLbl.AutoSize = true;
+            this.totalLbl.Location = new System.Drawing.Point(178, 319);
+            this.totalLbl.Name = "totalLbl";
+            this.totalLbl.Size = new System.Drawing.Size(70, 20);
+            this.totalLbl.TabIndex = 9;
+            this.totalLbl.Text = "Total: $0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.totalLbl);
+            this.Controls.Add(this.taxLbl);
+            this.Controls.Add(this.subtotalLbl);
+            this.Controls.Add(this.shirtCounterLbl);
+            this.Controls.Add(this.codeAcceptLbl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dcInput);
+            this.Controls.Add(this.orderBtn);
+            this.Controls.Add(this.qtInput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox qtInput;
+        private System.Windows.Forms.Button orderBtn;
+        private System.Windows.Forms.TextBox dcInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label codeAcceptLbl;
+        private System.Windows.Forms.Label shirtCounterLbl;
+        private System.Windows.Forms.Label subtotalLbl;
+        private System.Windows.Forms.Label taxLbl;
+        private System.Windows.Forms.Label totalLbl;
+    }
+}
+
---
+++ Hands On Test Assignments/CH06/HOT6P2/EX2/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EX2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        float shirtPrice = 13.75f;
+        float taxRate = 0.08f;
+
+        float[] discountCodes = { 8264, 5679, 6483 };
+
+        private float CheckDiscountCode(string code)
+        {
+           if (float.TryParse(code, out float discountCode))
+           {
+                for (int i = 0; i < discountCodes.Length; i++)
+                {
+                    if (discountCode == discountCodes[i])
+                    {
+                        if (discountCode == 8264)
+                        {
+                            return 0.30f;
+                        }
+                        if (discountCode == 5679)
+                        {
+                            return 0.20f;
+                        } 
+                        if (discountCode == 6483)
+                        {
+                            return 0.10f;
+                        }
+                    }
+                }
+
+                return 0.0f;
+           }
+           else
+           {
+               return 0.0f;
+           }
+        }
+
+        private void ShowInvoice(int quantity, float discountPercentage)
+        {
+            float subtotal;
+            float tax;
+            float total;
+            
+
+            if (discountPercentage == 0.0f)
+            {
+                shirtCounterLbl.Text = $"{quantity} T-Shirts @ $13.75 each";
+
+                subtotal = quantity * shirtPrice;
+                subtotalLbl.Text = $"Subtotal: ${subtotal}";
+
+                tax = taxRate * subtotal;
+                taxLbl.Text = $"Tax: ${tax}";
+
+                total = tax + subtotal;
+                totalLbl.Text = $"Total: ${total}";
+
+                codeAcceptLbl.Text = "Invalid Discount Code";
+            }
+            else
+            {
+                float discount = discountPercentage * 100;
+                codeAcceptLbl.Text = $"{discount}% Discount Applied!";
+
+
+                float newShirtPrice = shirtPrice - (shirtPrice * discountPercentage);
+
+                shirtCounterLbl.Text = $"{quantity} T-Shirts @ ${newShirtPrice} each";
+
+                subtotal = quantity * newShirtPrice;
+                subtotalLbl.Text = $"Subtotal: ${subtotal}";
+
+                tax = taxRate * subtotal;
+                taxLbl.Text = $"Tax: ${tax}";
+
+                total = tax + subtotal;
+                totalLbl.Text = $"Total: ${total}";
+            }
+        }
+
+        private void orderBtn_Click(object sender, EventArgs e)
+        {
+            int quantity = int.Parse(qtInput.Text);
+            string discountCode = dcInput.Text;
+
+            float userCode = CheckDiscountCode(discountCode);
+
+            ShowInvoice(quantity, userCode);
+        }
+    }
+}
