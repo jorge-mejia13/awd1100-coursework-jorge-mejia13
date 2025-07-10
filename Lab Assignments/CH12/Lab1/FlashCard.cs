b6333fd625e5c1912067b976983d6e853ad75a6c
jorge-mejia13 <jorge_mejia@insideranken.org>
2025-07-10 15:54:33 -0500

    Ch12 Lab1

---
+++ Lab Assignments/CH12/Lab1/FlashCard.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class FlashCard
+    {
+        // variables
+        private string _term;
+        private string _definition;
+
+        public string Term
+        {
+            get => _term;
+            set => _term = value;
+        }
+
+        public string Definition
+        {
+            get => _definition;
+            set => _definition = value;
+        }
+
+        //constructor
+        //public FlashCard(string term ,string definition)
+        //{
+        //    _term = term;
+        //    _definition = definition;
+        //}
+
+        //public void SetTerm(string term)
+        //{
+        //    _term = term;
+        //}
+        
+        //public void SetDefinition(string definition)
+        //{
+        //    _definition = definition;
+        //}
+
+        //public string GetTerm()
+        //{
+        //    return _term;
+        //}
+
+        //public string GetDefinition()
+        //{
+        //    return _definition;
+        //}
+
+    } 
+}
---
+++ Lab Assignments/CH12/Lab1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void defineBtn_Click(object sender, EventArgs e)
+        {
+            string term = termInput.Text;
+
+            FlashCard[] cards = new FlashCard[20];
+
+            for (int i = 0; i < cards.Length; i++)
+            {
+                cards[i] = new FlashCard();
+            }
+
+            cards[0].Definition = "A loop is...";
+            definitionLbl.Text = cards[0].Definition;
+        }
+    }
+}
